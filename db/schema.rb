8238d229d54076a68ff46b156450aab827c9fe70
Miranda Short <mird2794@gmail.com>
2019-01-08 13:50:26 -0500

    hashtags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_07_162853) do
+ActiveRecord::Schema.define(version: 2019_01_08_184950) do
+
+  create_table "tags", force: :cascade do |t|
+    t.string "phrase"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweet_tags", force: :cascade do |t|
+    t.integer "tweet_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string "message"
