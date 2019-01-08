d74ebda153b625b65934fdf5aca3bdce7cb3b50a
Miranda Short <mird2794@gmail.com>
2019-01-08 12:41:52 -0500

    prettier

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -57,7 +57,7 @@ class TweetsController < ApplicationController
   def destroy
     @tweet.destroy
     respond_to do |format|
-      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Tweet was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
